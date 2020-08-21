1c67c57595f010015a4d95744cae296fce74cc20
bhanujggandhi <gandhibhanuj@gmail.com>
2020-08-21 15:04:52 +0530

    Shop page and preview items component created

--- src/App.js
+++ src/App.js
@@ -2,18 +2,15 @@ import React from 'react';
 import './App.css';
 
 import Homepage from './pages/homepage/Homepage';
+import Shop from './pages/shop/Shop';
 import { Route, Switch } from 'react-router-dom';
-
-const HatsPage = () => {
-  return <h1>Hats Page</h1>;
-};
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={Homepage} />
-        <Route exact path='/hats' component={HatsPage} />
+        <Route exact path='/shop' component={Shop} />
       </Switch>
     </div>
   );
