3bb0fe7defededa9e1c601ad0d856a0d8d0f21d7
bhanujggandhi <gandhibhanuj@gmail.com>
2020-08-23 12:27:26 +0530

    Sign-in page, customInput, and customButton added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Homepage from './pages/homepage/Homepage';
 import Shop from './pages/shop/Shop';
 import { Route, Switch } from 'react-router-dom';
 import Header from './components/header/Header';
+import SignPage from './pages/singin-and-signup/SignPage';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route exact path='/shop' component={Shop} />
+        <Route exact path='/signin' component={SignPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/singin-and-signup/SignPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './SignPage.scss';
+import SignIn from '../../components/sign-in/SignIn';
+
+const SignPage = () => {
+  return (
+    <div className='sign-in-and-sign-up'>
+      <SignIn />
+    </div>
+  );
+};
+
+export default SignPage;
