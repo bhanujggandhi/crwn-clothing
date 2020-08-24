b0cb483b7ec4061ee536657b7c154b609045422f
bhanujggandhi <gandhibhanuj@gmail.com>
2020-08-24 15:26:25 +0530

    Sign up with email and password implemented

--- src/pages/singin-and-signup/SignPage.jsx
+++ src/pages/singin-and-signup/SignPage.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import './SignPage.scss';
 import SignIn from '../../components/sign-in/SignIn';
+import SignUp from '../../components/sign-up/SignUp';
 
 const SignPage = () => {
   return (
     <div className='sign-in-and-sign-up'>
       <SignIn />
+      <SignUp />
     </div>
   );
 };
