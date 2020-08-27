ef1427cba349f12b98d505e4a443338c6c6faea3
bhanujggandhi <gandhibhanuj@gmail.com>
2020-08-27 19:40:43 +0530

    added addItem and removeItem function to checkout page

--- src/components/checkout-item/CheckoutItem.jsx
+++ src/components/checkout-item/CheckoutItem.jsx
@@ -1,19 +1,51 @@
 import React from 'react';
 
+import { connect } from 'react-redux';
+import {
+  clearItemFromCart,
+  addItem,
+  removeItem,
+} from '../../redux/cart/cartActions';
+
 import './CheckoutItem.scss';
 
-const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => {
+const CheckoutItem = ({ cartItem, dispatch }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
   return (
     <div className='checkout-item'>
       <div className='image-container'>
         <img src={imageUrl} alt='item' />
       </div>
       <span className='name'>{name}</span>
-      <span className='quantity'>{quantity}</span>
+      <span className='quantity'>
+        <div onClick={() => dispatch(removeItem(cartItem))} className='arrow'>
+          &#10094;
+        </div>
+        <span className='value'>{quantity}</span>
+        <div onClick={() => dispatch(addItem(cartItem))} className='arrow'>
+          &#10095;
+        </div>
+      </span>
       <span className='price'>{price}</span>
-      <div className='remove-button'>&#10005</div>
+      <div
+        onClick={() => dispatch(clearItemFromCart(cartItem))}
+        className='remove-button'
+      >
+        &#10005;
+      </div>
     </div>
   );
 };
 
-export default CheckoutItem;
+export default connect()(CheckoutItem);
+
+//We have used shorthand for mapDispatchToProps, that is if we connect HOC to our component, then we have 'dispatch' function in props. So we can call
+// onClick={() => dispatch(clearItemFromCart(cartItem))}
+
+// It is equivalent to
+// const mapDispatchToProps = (dispatch) => ({
+//   removeItem: (item) => dispatch(clearItemFromCart(item)),
+// });
+// export default connect(null, mapDispatchToProps)(CheckoutItem);
+// and then,
+// onClick={() => removeItem(cartItem)}
--- src/redux/cart/cartReducer.js
+++ src/redux/cart/cartReducer.js
@@ -1,4 +1,4 @@
-import { addItemToCart } from './cartUtils';
+import { addItemToCart, removeItemFromCart } from './cartUtils';
 const { cartActionTypes } = require('./cartTypes');
 
 const INITIAL_STATE = {
@@ -17,6 +17,18 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload),
+      };
+    case cartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload),
+      };
+    case cartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          (cartItem) => cartItem.id !== action.payload.id
+        ),
       };
     default:
       return state;
